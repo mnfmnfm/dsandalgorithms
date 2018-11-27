bbea92c31ecd6ec5b3feb06bcd7f3caad9f68852
mnfmnfm <mnfmnfm@gmail.com>
2018-11-26 16:35:06 -0800

    initial commit

---
+++ src/main/java/ArrayReverse.java
@@ -0,0 +1,11 @@
+public class ArrayReverse {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        int result = addition(1,1);
+        System.out.println("addition(1,1) gave back " + result);
+    }
+
+    public static int addition(int a, int b) {
+        return a + b;
+    }
+}
