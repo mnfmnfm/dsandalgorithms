3ac9944f3596dcad6ec43d7b309a639fe9b6336e
mnfmnfm <mnfmnfm@gmail.com>
2018-11-29 13:43:57 -0800

    add array reverse test

---
+++ src/test/java/ArrayReverseTest.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ArrayReverseTest {
+
+    @Test
+    public void testAddition() {
+        assertTrue(ArrayReverse.addition(1,1) == 2);
+    }
+}
